7538021643bdd730187be4d24892b94c4d6b591d
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-08-01 20:51:38 +0200

    first commit

---
+++ src/Controller/EmployeController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Employe;
+use App\Form\EmployeType;
+use App\Repository\EmployeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class EmployeController extends AbstractController
+{
+    #[Route('/employe', name: 'app_employe')]
+    public function index(EmployeRepository $employeRepository): Response
+    {
+        // SELECT * FROM employe ORDER BY nom ASC
+        $employes = $employeRepository->findBy([],["nom" => "ASC"]);
+        return $this->render('employe/index.html.twig', [
+            'employes'=>$employes
+        ]);
+    }
+
+    #[Route('/employe/new', name: 'new_employe')]
+    #[Route('/employe/{id}/edit', name: 'edit_employe')]
+    public function new_edit(Employe $employe = null, Request $request, EntityManagerInterface $entityManager): Response
+    { if (!$employe) {
+        $employe = new Employe();
+    }
+    
+        $form = $this->createForm(EmployeType::class, $employe);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $employe = $form->getData();
+            $entityManager->persist($employe);
+            $entityManager->flush();
+
+
+            return $this->redirectToRoute('app_employe');
+        }
+
+        return $this->render('employe/new.html.twig', [
+            'formAddEmploye' => $form,
+        ]);
+    }
+
+
+    #[Route('/employe/{id}', name: 'show_employe')]
+    public function show(Employe $employe): Response
+    {
+        return $this->render('employe/show.html.twig', [
+            'employe'=>$employe
+        ]); 
+    }
+}
---
+++ src/Controller/EntrepriseController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Entreprise;
+use App\Form\EntrepriseType;
+use App\Repository\EntrepriseRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class EntrepriseController extends AbstractController
+{
+    #[Route('/entreprise', name: 'app_entreprise')]
+
+    // public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntrepriseRepository $entrepriseRepository): Response
+
+    {
+    // $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
+    // $entreprises = $entrepriseRepository->findAll();
+    // SELECT * FROM entreprise WHERE VILLE = 'Strasbourg' ORDER BY raisonSociale ASC
+    $entreprises = $entrepriseRepository->findBy([],["raisonSociale" => "ASC"]);
+
+        return $this->render('entreprise/index.html.twig', [
+
+            'entreprises'=>$entreprises
+        ]);
+
+    }
+
+    #[Route('/entreprise/new', name: 'new_entreprise')]
+    #[Route('/entreprise/{id}/edit', name: 'edit_entreprise')]
+
+    public function new_edit(Entreprise $entreprise = null, Request $request,EntityManagerInterface $entityManager): Response
+    {
+        if(!$entreprise) {
+            $entreprise = new Entreprise();
+        }
+
+        $form = $this->createForm(EntrepriseType::class, $entreprise);
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entreprise = $form->getData();
+            //prepare PDO
+            $entityManager->persist($entreprise);
+            //execute PDO
+            $entityManager->flush();
+
+
+            return $this->redirectToRoute('app_entreprise');
+        }
+
+        return $this->render('entreprise/new.html.twig', [
+            'formAddEntreprise' => $form,
+            'edit'=> $entreprise->getId()
+        ]);
+    }
+
+
+    #[Route('/entreprise/{id}', name: 'show_entreprise')]
+    public function show(Entreprise $entreprise): Response
+    {
+        return $this->render('entreprise/show.html.twig', [
+            'entreprise'=>$entreprise
+        ]); 
+    }
+}
